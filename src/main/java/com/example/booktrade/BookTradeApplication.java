24361c76235ba243e6d70b814c248376ee84d498
Daniel Dąbrowski <danieldabrowskikontakt@gmail.com>
2024-01-29 08:26:19 +0100

    project init

---
+++ src/main/java/com/example/booktrade/BookTradeApplication.java
@@ -0,0 +1,13 @@
+package com.example.booktrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookTradeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookTradeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/booktrade/BookTradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.booktrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookTradeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
